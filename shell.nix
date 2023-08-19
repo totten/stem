382c0e61cf73de34c521faad741d9188dd7a9969
Tim Otten <totten@civicrm.org>
2023-08-19 16:44:47 -0700

    shell.nix - Update buildkit. Add more comments.

--- shell.nix
+++ shell.nix
@@ -7,42 +7,47 @@
 let
 
   buildkit = import (pkgs.fetchFromGitHub {
-    owner = "totten";
+    owner = "civicrm";
     repo = "civicrm-buildkit";
-    rev = "153371e9bdcb22392b878cca545df0888fb61925";
-    sha256 = "sha256-rdwmA4uqIqfqXu2f+ewVH0Gs/BzcB13p8oRbbTdUsAs=";
+    rev = "04b338a52bbf0bdc21edafe564b875138c1db12c";
+    sha256 = "sha256-zTt9TZ7fZ+xjDicTfRiEalnK4Y8ijd89cN1sT8FA1eQ=";
     ## TODO: See if we can track master without needing to lock-in revisions
   });
 
   ## If you're trying to patch buildkit at the sametime, then use a local copy:
   # buildkit = import ((builtins.getEnv "HOME") + "/buildkit/default.nix");
   # buildkit = import ((builtins.getEnv "HOME") + "/bknix/default.nix");
-  
-  fetchPhar = buildkit.funcs.fetchPhar;
 
 in
 
+  ## Now, we define a shell with the packages that we want. This may use:
+  ##
+  ## Profiles (curated set of packages used by CI)
+  ##   buildkit.profiles.min  (Low versions of PHP/MySQL)
+  ##   buildkit.profiles.max  (High versions of PHP/MySQL)
+  ##   buildkit.profiles.edge (Future versions of PHP/MySQL)
+  ##   buildkit.profiles.dfl
+  ##   buildkit.profiles.alt
+  ##
+  ## Specific packages (from buildkit)
+  ##   buildkit.pkgs.php73
+  ##   buildkit.pkgs.php81
+  ##   buildkit.pkgs.composer
+  ##
+  ## Specific packages (from upstream releases of nixpkgs)
+  ##   buildkit.dists.v2205.composer
+  ##   buildkit.dists.v2305.composer
+  ##   buildkit.dists.v2305.php81
+
   pkgs.mkShell {
     nativeBuildInputs = buildkit.profiles.dfl ++ [
-
       buildkit.pkgs.composer
       buildkit.pkgs.pogo
       buildkit.pkgs.loco
       buildkit.pkgs.phpunit8
       buildkit.pkgs.phpunit9
-      
-      ## TODO: Move these to buildkit:nix/pkgs/default.nix
-      (fetchPhar {
-        name = "cv";
-        url = "https://download.civicrm.org/cv/cv-0.3.47.phar";
-        sha256 = "sha256-q/pzWbB8IhD/rV3d/o3nQCNXc9RTGoG02cq7+YXUkc4=";
-      })
-      (fetchPhar {
-        name = "civix";
-        url = "https://download.civicrm.org/civix/civix-23.08.0.phar";
-        sha256 = "sha256-HaniHKvnI406AwDU696/abpAmnhUT7y4vnrg7K6jMV0=";
-      })
-
+      buildkit.pkgs.cv
+      buildkit.pkgs.civix
       pkgs.bash-completion
     ];
     shellHook = ''
